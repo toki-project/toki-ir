1af72430e19f7c982316fd694ee0cfb5909415aa
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-08-07 16:58:05 -0400

    chore: Add Ruff to pre-commit

--- src/arxir/ast/__init__.py
+++ src/arxir/ast/__init__.py
@@ -1,8 +1,9 @@
-from astx.base import *
-from arxir.ast.blocks import *
-from astx.callables import *
-from astx.datatypes import *
-from astx.mixes import *
-from astx.modifiers import *
-from astx.operators import *
-from astx.statements import *
+from astx.base import *  # noqa: F403
+from arxir.ast.blocks import *  # noqa: F403
+from astx.callables import *  # noqa: F403
+from astx.datatypes import *  # noqa: F403
+from astx.flows import *  # noqa: F403
+from astx.mixes import *  # noqa: F403
+from astx.modifiers import *  # noqa: F403
+from astx.operators import *  # noqa: F403
+from astx.variables import *  # noqa: F403
